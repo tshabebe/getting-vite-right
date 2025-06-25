2d8d134c6feaf6c2e8d670f539a57f8751959b0c
Teshome Abebe <161710654+tshabebe@users.noreply.github.com>
2025-06-25 15:49:10 +0000

    refactor: clean up formatting and remove unused imports in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,45 @@
 'use client'
-import { useNavigate } from "react-router";
-import {
-  createBrowserRouter,
-  NavLink,
-  RouterProvider,
-} from "react-router";
-import { paths } from "./paths";
-import Home from "./home";
+import { createBrowserRouter, NavLink, RouterProvider } from 'react-router'
+import { paths } from './paths'
+import Home from './home'
 
 const router = createBrowserRouter([
   {
     path: paths.app.support.getHref(),
-    element: <div><div>Hello from support</div> <OnClickNavigation /></div>,
+    element: (
+      <div>
+        <div>Hello from support</div> <OnClickNavigation />
+      </div>
+    ),
   },
   {
-    path: "/",
+    path: '/',
     element: <Home />,
   },
   {
-    path: "/test",
-    element: <div>hello from <NavLink to={'/support'}>test</NavLink></div>,
+    path: '/test',
+    element: (
+      <div>
+        hello from <NavLink to={'/support'}>test</NavLink>
+      </div>
+    ),
   },
   {
-    path: "*",
+    path: '*',
     element: <div>404 - Not Found</div>,
   },
-]);
+])
 
 function OnClickNavigation() {
-    const navigate = useNavigate();
-
-  return <button onClick={() => window.location.href = "https://google.com" }>button</button>
+  return (
+    <button onClick={() => (window.location.href = 'https://google.com')}>
+      button
+    </button>
+  )
 }
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
